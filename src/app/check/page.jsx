22057e3ce55f2a92e55affeb8692d132937e51a5
FarhanShafqut <farhanshafqut869@gmail.com>
2023-12-10 12:31:36 +0000

    first commit

---
+++ src/app/check/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {billList} from "src/app/(components)/BillList.jsx"
+
+const page = () => {
+  return (
+    <div>
+        <billList/>
+    </div>
+  )
+}
+
+export default page
